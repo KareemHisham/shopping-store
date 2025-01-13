b8ef97f44a3a8f35dabe1e4fff0fcc34af674356
Reda Elemawy <redaelemawy98@gmail.com>
2025-01-13 02:48:18 +0200

    Inspiration section heading

---
+++ src/components/sections/inspiration/Left.tsx
@@ -0,0 +1,18 @@
+const InspirationHeading = () => {
+  return (
+    <div className="flex flex-col ps-10 justify-center h-full w-full md:w-5/12 ">
+      <h2 className="text-xxl font-bold leading-10">
+        50+ Beautiful rooms inspiration
+      </h2>
+      <p className="w-10/12">
+        Our designer already made a lot of beautiful prototipe of rooms that
+        inspire you
+      </p>
+      <button className="bg-primary text-base text-white w-1/2 p-2 mt-8">
+        Explore More
+      </button>
+    </div>
+  );
+};
+
+export default InspirationHeading;
--- src/components/sections/inspiration/index.tsx
+++ src/components/sections/inspiration/index.tsx
@@ -1,5 +1,14 @@
+import InspirationHeading from './Left';
+
 const Inspiration = () => {
-  return <section className="w-full min-h-[50vw] bg-tertiary"></section>;
+  return (
+    <section className="w-full h-auto  md:h-[50vw] bg-tertiary pt-5">
+      <div className="flex h-full">
+        <InspirationHeading />
+        <div className="flex-1 bg-slate-200">right</div>
+      </div>
+    </section>
+  );
 };
 
 export default Inspiration;
