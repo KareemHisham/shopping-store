f2aff1522d848cb73a74a66b6be9df7493b034c5
Reda Elemawy <redaelemawy98@gmail.com>
2025-01-13 16:07:13 +0200

    inspiration list  responsive

--- src/components/sections/inspiration/index.tsx
+++ src/components/sections/inspiration/index.tsx
@@ -4,7 +4,7 @@ import InspirationList from './List';
 const Inspiration = () => {
   return (
     <section className="w-full h-[100vh] md:h-[50vw] bg-tertiary py-5">
-      <div className="flex h-full flex-col md:flex-row">
+      <div className="flex h-full flex-col md:flex-row gap-5">
         <InspirationHeading />
         <InspirationList />
       </div>
