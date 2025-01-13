4e8dee6043c05eb390c4c8f455fbd30de79054b8
Reda Elemawy <redaelemawy98@gmail.com>
2025-01-13 15:48:40 +0200

    inspiration-list progress

--- src/components/sections/inspiration/List.tsx
+++ src/components/sections/inspiration/List.tsx
@@ -16,11 +16,12 @@ const InspirationList = () => {
           <div className="w-[300px] h-full bg-indigo-100 flex-shrink-0"></div>
         </div>
         {/** status */}
-        <div className="flex justify-start items-center gap-3 h-1/6">
-          <span className="w-4 h-4 bg-primary"></span>
-          <span className="w-4 h-4 bg-slate-200"></span>
-          <span className="w-4 h-4 bg-slate-200"></span>
-          <span className="w-4 h-4 bg-slate-200"></span>
+        <div className="flex justify-start items-center gap-3 h-1/6 ms-3">
+          <span className="w-3 h-3 bg-primary rounded-full outline outline-1 outline-offset-8 outline-primary me-2"></span>
+
+          <span className="w-3 h-3 bg-[#D8D8D8] rounded-full"></span>
+          <span className="w-3 h-3 bg-[#D8D8D8] rounded-full"></span>
+          <span className="w-3 h-3 bg-[#D8D8D8] rounded-full"></span>
         </div>
       </div>
     </div>
