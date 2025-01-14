e79863272b0c2a19a857e55d4552fc7ab6fa2cfc
Reda Elemawy <redaelemawy98@gmail.com>
2025-01-14 16:22:44 +0200

    init product page

--- src/root/pages/ProductDetails.tsx
+++ src/root/pages/ProductDetails.tsx
@@ -1,7 +1,16 @@
+import { useParams } from 'react-router-dom';
+import { MiniHero } from '../../components/Index';
+import { PRODUCTS } from '../../constant/Index';
 const ProductDetails = () => {
+  const { id } = useParams();
+  const product = PRODUCTS.find((product) => product.id === 1);
   return (
-    <div>ProductDetails</div>
-  )
-}
+    <>
+      <MiniHero>
+        <h1 className="text-xl font-bold tracking-[5px]">Details</h1>
+      </MiniHero>
+    </>
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
