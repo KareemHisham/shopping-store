73d09cafe5bfa7f870a74a4b7829d2650ef3eb23
Reda Elemawy <redaelemawy98@gmail.com>
2025-01-14 19:01:42 +0200

    product_overview--- product images layout

---
+++ src/components/sections/product-overview/ProductImages.tsx
@@ -0,0 +1,17 @@
+const ProductImages = () => {
+  return (
+    <div className="w-full md:w-5/12  flex gap-4 h-4/6">
+      {/** small imgs */}
+      <div className="flex flex-col gap-2 w-[15%]">
+        <div className="bg-orange-300 w-[100%] h-[18%] rounded-lg"></div>
+        <div className="bg-orange-300 w-[100%] h-[18%] rounded-lg"></div>
+        <div className="bg-orange-300 w-[100%] h-[18%] rounded-lg"></div>
+        <div className="bg-orange-300 w-[100%] h-[18%] rounded-lg"></div>
+      </div>
+      {/** active img */}
+      <div className="bg-violet-800 w-[70%] h-full rounded-lg"></div>
+    </div>
+  );
+};
+
+export default ProductImages;
