78efa916ff5d335b295ce2926ab848b5f9ae2962
Reda Elemawy <redaelemawy98@gmail.com>
2025-01-16 01:50:55 +0200

    product_overview---edit actions buttons font-size in small screens

--- src/components/sections/product-overview/ProductActions.tsx
+++ src/components/sections/product-overview/ProductActions.tsx
@@ -1,15 +1,15 @@
 const Actions = () => {
   return (
     <div className="flex gap-2 ">
-      <div className="flex gap-2 justify-between md:w-[20%] border-2 border-gray-400 rounded-md p-3 text-sm md:text-base">
+      <div className="flex gap-2 justify-between lg:w-[20%] border-2 border-gray-400 rounded-md p-3 text-sm lg:text-base">
         <span className="cursor-pointer font-bold">-</span>
         <span>1</span>
         <span className="cursor-pointer font-bold">+</span>
       </div>
-      <button className="md:w-[30%] border-2 border-black rounded-md p-3 text-sm md:text-base">
+      <button className="lg:w-[30%] border-2 border-black rounded-md p-3 text-sm lg:text-base">
         Add To Cart
       </button>
-      <button className="md:w-[30%] border-2 border-black rounded-md p-3 text-sm md:text-base">
+      <button className="lg:w-[30%] border-2 border-black rounded-md p-3 text-sm lg:text-base">
         +Compare
       </button>
     </div>
