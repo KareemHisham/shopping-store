096072321a324d954f898203e128827e42fdb716
Reda Elemawy <redaelemawy98@gmail.com>
2025-01-16 01:41:50 +0200

    product_overview--- actions buttons widths in small screens

--- src/components/sections/product-overview/ProductActions.tsx
+++ src/components/sections/product-overview/ProductActions.tsx
@@ -1,15 +1,15 @@
 const Actions = () => {
   return (
     <div className="flex gap-2 ">
-      <div className="flex gap-2 justify-between w-[20%] border-2 border-gray-400 rounded-md p-3">
+      <div className="flex gap-2 justify-between md:w-[20%] border-2 border-gray-400 rounded-md p-3 text-sm md:text-base">
         <span className="cursor-pointer font-bold">-</span>
         <span>1</span>
         <span className="cursor-pointer font-bold">+</span>
       </div>
-      <button className="w-[30%] border-2 border-black rounded-md p-3">
+      <button className="md:w-[30%] border-2 border-black rounded-md p-3 text-sm md:text-base">
         Add To Cart
       </button>
-      <button className="w-[30%] border-2 border-black rounded-md p-3">
+      <button className="md:w-[30%] border-2 border-black rounded-md p-3 text-sm md:text-base">
         +Compare
       </button>
     </div>
