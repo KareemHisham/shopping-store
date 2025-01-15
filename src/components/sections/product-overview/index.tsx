d48a35179d9b4bfa454dbc0521c6567efbac5e4e
Reda Elemawy <redaelemawy98@gmail.com>
2025-01-16 01:57:36 +0200

    product_overview--finalizing--clearing--unmeaningfull--comments

--- src/components/sections/product-overview/index.tsx
+++ src/components/sections/product-overview/index.tsx
@@ -13,7 +13,6 @@ const ProductOverview = ({ product }: { product: IProduct }) => {
         <ProductImages />
         <div className="w-full  md:w-7/12 h-4/6 md:h-full">
           <div className="h-4/6 flex flex-col md:justify-between gap-2 md:gap-0">
-            {/** heading */}
             <div>
               <h1 className="text-xl font-bold">Asgaard sofa</h1>
               <p className="text-gray-300">Rs. 250,000.00</p>
@@ -23,25 +22,21 @@ const ProductOverview = ({ product }: { product: IProduct }) => {
                 <p className="text-gray-300">5 Customer Review</p>
               </div>
             </div>
-            {/**description */}
             <p className="text-sm">
               Setting the bar as one of the loudest speakers in its class, the
               Kilburn is a compact, stout-hearted hero with a well-balanced
               audio which boasts a clear midrange and extended highs for a
               sound.
             </p>
-            {/** sizes */}
+
             <Sizes sizes={['XL', 'L', 'SM']} active="XL" />
-            {/** colors */}
+
             <ProductColors
               colors={['#816DFA', '#000000', '#B88E2F']}
               active="#B88E2F"
             />
-            {/** actions */}
             <Actions />
           </div>
-
-          {/** info */}
           <ProductSummary />
         </div>
       </div>
